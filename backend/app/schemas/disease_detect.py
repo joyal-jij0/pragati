0080c2b3a417727348524c52bd42069d261af2f9
Joyal Jijo <joyal2405@gmail.com>
2025-04-29 10:58:41 +0530

    integrated llm to plant disease

--- backend/app/schemas/disease_detect.py
+++ backend/app/schemas/disease_detect.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel 
-from typing import Dict 
+from typing import Dict, Any
 
 class DetectionResponse(BaseModel): 
     class_counts: Dict[str, int] 
+    llm_response: Any
--- backend/app/services/disease_detect_service.py
+++ backend/app/services/disease_detect_service.py
@@ -4,6 +4,8 @@ import cv2
 import numpy as np 
 from ultralytics import YOLO 
 import supervision as sv 
+from groq import Groq 
+import json 
 
 logger = logging.getLogger(__name__) 
 
@@ -51,3 +53,32 @@ def detect_image(path: str) -> dict[str, int]:
 
     logger.debug(f"Detection counts: {counts}")
     return counts 
+
+def get_llm_response(class_counts: dict[str, int]) -> dict: 
+    client = Groq() 
+    user_content = json.dumps({"class counts": class_counts}, indent=4)
+    completion = client.chat.completions.create(
+        model = "meta-llama/llama-4-maverick-17b-128e-instruct",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are the best Indian Agricultural Crop Doctor in Town. You are provided the crop diseases with its no in a json format you are to output Disease Details in one line, symptoms in 3 points, causes in 3 points and treatment in 3 points. All in a json format. If there are multiple diseases still provide one json with these only."
+            },
+            {
+                "role": "user",
+                "content": user_content
+            }
+        ],
+        temperature=1, 
+        max_completion_tokens=1024,
+        top_p=1, 
+        stream=False, 
+        response_format={"type": "json_object"},
+        stop=None, 
+    )
+    llm_response = completion.choices[0].message.content 
+    
+    try:
+        return json.loads(llm_response) 
+    except Exception: 
+        return {"errror": "Failed to parse LLM response", "raw": llm_response}
