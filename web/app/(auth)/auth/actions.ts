708b91bfddb304931764ca187763c1622e5d1af0
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-05-01 12:55:14 +0530

    refactor: restructure authentication components and integrate Supabase for session management

---
+++ web/app/(auth)/auth/actions.ts
@@ -0,0 +1,73 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/utils/supabase/server'
+import type { SignUpWithPasswordCredentials } from '@supabase/supabase-js'
+import { headers } from 'next/headers'
+
+export async function login(formData: FormData) {
+  const supabase = await createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signInWithPassword(data)
+
+  if (error) {
+    redirect('/error')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
+
+export async function signup(formData: FormData) {
+  const supabase = await createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+    options: {
+      data: {
+        display_name: formData.get('name') as string,
+      },
+    },
+  } satisfies SignUpWithPasswordCredentials
+
+  const { error } = await supabase.auth.signUp(data)
+
+  if (error) {
+    redirect('/error')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
+
+export async function signInWithGithub() {
+  const supabase = await createClient()
+  const origin = (await headers()).get('origin')
+
+  const { error, data } = await supabase.auth.signInWithOAuth({
+    provider: 'github',
+    options: {
+      redirectTo: `${origin}/auth/callback`,
+    },
+  })
+
+  if (error) {
+    redirect('/error')
+  }
+
+  if (data.url) {
+    redirect(data.url) // use the redirect API for your server framework
+  }
+}
--- web/app/(auth)/auth/page.tsx
+++ web/app/(auth)/auth/page.tsx
@@ -1,9 +1,9 @@
 import { LoginForm } from '@/components/login-form'
-import { GalleryVerticalEnd, TreePalmIcon } from 'lucide-react'
+import { TreePalmIcon } from 'lucide-react'
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10  bg-muted">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="bg-green-500 flex h-7 w-7 items-center justify-center rounded-md text-primary-foreground">
