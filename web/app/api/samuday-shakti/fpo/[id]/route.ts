12598c09d4d9db5a6e338acfeccad88b095530e7
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-05-04 16:15:59 +0530

    Update route.ts

--- web/app/api/samuday-shakti/fpo/[id]/route.ts
+++ web/app/api/samuday-shakti/fpo/[id]/route.ts
@@ -3,7 +3,10 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
   try {
     const id = params.id
 
